bd462edb01c727c8ce9aff13eb2639a3bfb9d1b4
Maddie Woolley <95057458+maddie-woolley@users.noreply.github.com>
2022-09-06 17:17:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
 
+warnings.filterwarnings('ignore')
+sns.set(color_codes=True)
+
+weather = pd.read_csv('weather.csv')
+weather.head()
+weather.describe()
+weather.isnull()
+weather.isna()
+
+plt.figure(figsize=(10, 5))
+plt.title('BAR PLOT')
+sns.barplot(data=weather, x="humidity", y="temperature")
+plt.show()
+
+plt.figure(figsize=(10, 5))
+plt.title('HIST PLOT')
+sns.distplot(weather['humidity'])
+plt.show()
+
+plt.figure(figsize=(10, 5))
+plt.title('HIST PLOT')
+sns.distplot(weather['humidity'], kde=False, rug=True);
+plt.show()
+
+sns.pairplot(weather[['humidity', 'temperature', 'air_pollution_index']])
+plt.show()
+
+sns.stripplot(data=weather, x="weather_type", y="temperature")
+plt.figure(figsize=(10, 5))
+plt.title('BOX PLOT')
+sns.boxplot(data=weather, x="humidity", y="temperature", hue="weather_type")
+plt.show()
+
+plt.figure(figsize=(10, 5))
+plt.title('COUNT PLOT')
+sns.countplot(x=weather['weather_type'])
+plt.show()
+
+sns.pointplot(data=weather, x="humidity", y="temperature", hue="weather_type")
+plt.figure(figsize=(10,5))
+plt.title('REG PLOT')
+sns.regplot(x="humidity", y="temperature", data=weather)
+plt.show()
